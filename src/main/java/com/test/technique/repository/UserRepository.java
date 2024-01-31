54cc1a0d5cc1c3023d29d27f96c8327aab1c7c74
DavidBlas <dmorales@servifacil.com.mx>
2024-01-31 01:59:25 -0600

    feat: Add repository to user entity

---
+++ src/main/java/com/test/technique/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.test.technique.repository;
+
+import com.test.technique.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
